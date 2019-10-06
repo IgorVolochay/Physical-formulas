a299f90b05b1f16c93d80b8bf2b6eea9ba3ac285
Igor Volochay <44619012+IgorVolochay@users.noreply.github.com>
2019-10-06 16:51:22 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+import math
+import time
+
+import numpy
+
+form = input("Please, enter the name of the formula \n(or enter the \"help\" to view the available formulas):\n")
+
+if form == "help":
+    print(  )
