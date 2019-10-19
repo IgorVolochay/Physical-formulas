7c1a82cf9bf19ef0132fb4bfe6951645cf180df7
Igor Volochay <44619012+IgorVolochay@users.noreply.github.com>
2019-10-19 08:57:44 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,13 @@ import time
 
 import numpy
 
-form = input("Please, enter the name of the formula \n(or enter the \"help\" to view the available formulas):\n")
+while True:
 
-if form == "help":
-    print(  )
+    form = input("\nPlease, enter the name of the formula \n(or enter the \"help\" to view the available formulas):\n")
+
+    if form == "help": #Shows available formulas
+        print("  Available formulas:")
+        print("- The main\n")
+
+    if form == "The main" or "the main":
+        print("- Speed\n- Time\n- Distance")
